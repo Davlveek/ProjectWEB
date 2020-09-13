2b6ef9f52472bc878966dbea3ae1f69151674714
Davlveek <stankeev.vladimir@gmail.com>
2020-09-13 23:27:23 +0300

    added serializer class for message model

--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 from .models import User
+from .models import MessageModel
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.serializers import ModelSerializer, CharField
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['name', 'age', 'gender', 'first_name', 'last_name']
+
+
+class MessageModelSerializer(ModelSerializer):
+    user = CharField(source='user.username', read_only=True)
+    recipient = CharField(source='recipient.username')
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        recipient = get_object_or_404(User, username=validated_data['recipient']['username'])
+        msg = MessageModel(recipient=recipient,
+                           body=validated_data['body'],
+                           user=user)
+        msg.save()
+        return msg
+
+    class Meta:
+        model = MessageModel
+        fields = ('id', 'user', 'recipient', 'timestamp', 'body')
