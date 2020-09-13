bd89134996e2f7a245de5767c82b2dadbab5a3d8
Davlveek <stankeev.vladimir@gmail.com>
2020-09-13 23:27:47 +0300

    added api file for drf

---
+++ backend/chat/api.py
@@ -0,0 +1,16 @@
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication
+
+from .serializers import MessageModelSerializer
+from .models import MessageModel
+
+
+class MessageModelViewSet(ModelViewSet):
+    queryset = MessageModel.objects.all()
+    serializer_class = MessageModelSerializer
+    allowed_methods = ('GET', 'POST', 'HEAD', 'OPTIONS')
