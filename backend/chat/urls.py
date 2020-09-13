25c7d7b7110f35da451fd5b93abe862f25cd409d
Davlveek <stankeev.vladimir@gmail.com>
2020-09-13 23:28:07 +0300

    added api url for chat

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from rest_framework.routers import DefaultRouter
+from .api import MessageModelViewSet
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+router = DefaultRouter()
+router.register(r'message', MessageModelViewSet, basename='message-api')
 
 urlpatterns = [
     # Get or set info about currently authenticated user
@@ -16,4 +23,7 @@ urlpatterns = [
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/logout/', views.LogoutView.as_view(), name='logout'),
     path('api/auth/signup/', views.SignupView.as_view(), name='signup'),
+
+    # Chat
+    path(r'api/v1', include(router.urls)),
 ]
