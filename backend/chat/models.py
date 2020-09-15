1e3c8b570363fd45c2e92adc7221c343096d8ba7
Dmitry Mukhachev <dmitry.mukhachev@netcracker.com>
2020-09-16 00:03:38 +0300

    add id to user model

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -3,21 +3,28 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.hashers import make_password
 
 class UserManager(BaseUserManager):
-    def create_user(self, name, age, gender, password=None):
-        user = self.model(name=name, age=age, gender=gender)
+    next_id = 1
+    def getNextId(self):
+        to_return = self.next_id
+        self.next_id += 1
+        return to_return
+
+    def create_user(self, login, age, gender, password=None):
+        user = self.model(id=self.getNextId(), login=login, age=age, gender=gender)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, name, age, gender, password=None):
-        user = self.create_user(name, age, gender, password)
+    def create_superuser(self, login, age, gender, password=None):
+        user = self.create_user(0, login, age, gender, password)
         user.is_admin = True
         user.save()
         return user
 
 
 class User(AbstractBaseUser):
-    name = models.TextField(unique=True)
+    id = models.TextField(primary_key=True, unique=True)
+    login = models.TextField(unique=True)
     first_name = models.TextField(blank=True)
     last_name = models.TextField(blank=True)
     age = models.IntegerField()
@@ -36,4 +43,7 @@ class User(AbstractBaseUser):
     REQUIRED_FIELDS = ['age', 'gender']
 
     def __str__(self):
-        return f'{self.name}, {self.age} years old, {self.get_gender_display()}'
+        return f'{self.login}, {self.age} years old, {self.get_gender_display()}'
+
+    def get_partners(self, partners_list):
+        self.partners_list = partners_list
