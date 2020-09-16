dd680f088a82e4e8a51edaf136d52b3ac3aa0cb8
root <root@ip-172-31-24-153.eu-north-1.compute.internal>
2020-09-16 16:02:12 +0000

    add host and disablehostchek to vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,8 @@ module.exports = {
     "vuetify"
   ],
   devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true,
     proxy: {
       '^/api/': {
         target: 'http://127.0.0.1:8000/api/',
