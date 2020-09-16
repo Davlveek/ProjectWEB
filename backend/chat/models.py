5115a788ce8afc504c77b1a52f5e1a87c0db81c9
Dmitry Mukhachev <dmitry.mukhachev@netcracker.com>
2020-09-16 17:03:54 +0300

    changed name to login

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'name'
+    USERNAME_FIELD = 'login'
     REQUIRED_FIELDS = ['age', 'gender']
 
     def __str__(self):
