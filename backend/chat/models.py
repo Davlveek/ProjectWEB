cf84091340b41da37d3f497d3e3b5f9310ec9dcd
Dmitry Mukhachev <dmitry.mukhachev@netcracker.com>
2020-09-16 22:29:27 +0300

    Revert "add id to user model"
    This reverts commit 1e3c8b57

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -3,28 +3,21 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.hashers import make_password
 
 class UserManager(BaseUserManager):
-    next_id = 1
-    def getNextId(self):
-        to_return = self.next_id
-        self.next_id += 1
-        return to_return
-
-    def create_user(self, login, age, gender, password=None):
-        user = self.model(id=self.getNextId(), login=login, age=age, gender=gender)
+    def create_user(self, name, age, gender, password=None):
+        user = self.model(name=name, age=age, gender=gender)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, login, age, gender, password=None):
-        user = self.create_user(0, login, age, gender, password)
+    def create_superuser(self, name, age, gender, password=None):
+        user = self.create_user(name, age, gender, password)
         user.is_admin = True
         user.save()
         return user
 
 
 class User(AbstractBaseUser):
-    id = models.TextField(primary_key=True, unique=True)
-    login = models.TextField(unique=True)
+    name = models.TextField(unique=True)
     first_name = models.TextField(blank=True)
     last_name = models.TextField(blank=True)
     age = models.IntegerField()
@@ -43,7 +36,4 @@ class User(AbstractBaseUser):
     REQUIRED_FIELDS = ['age', 'gender']
 
     def __str__(self):
-        return f'{self.login}, {self.age} years old, {self.get_gender_display()}'
-
-    def get_partners(self, partners_list):
-        self.partners_list = partners_list
+        return f'{self.name}, {self.age} years old, {self.get_gender_display()}'
