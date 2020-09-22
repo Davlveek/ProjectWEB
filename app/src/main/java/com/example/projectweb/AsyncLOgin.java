810211fbaabce5303ac7aac49d0c4a2b80f22949
klimov-andre <klimandr1998@gmail.com>
2020-09-22 22:26:06 +0300

    Исправлен логин

--- app/src/main/java/com/example/projectweb/AsyncLOgin.java
+++ app/src/main/java/com/example/projectweb/AsyncLOgin.java
@@ -2,36 +2,108 @@ package com.example.projectweb;
 
 import android.os.AsyncTask;
 
-import com.example.projectweb.chat.User;
-import com.example.projectweb.rest.RestApi;
-import com.example.projectweb.rest.Retrorest;
+import com.example.projectweb.chat.LOgin;
 
-import retrofit2.Call;
-import retrofit2.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 // Вернет или пустой токен или нормальный токен
-public class AsyncLOgin extends AsyncTask<User, Void, Token> {
+public class AsyncLOgin extends AsyncTask<LOgin, Void, Token> {
+    private static final Object BASE_URL = "https://rev-o-dates.social:8000";
     private Exception exception;
 
-    protected Token doInBackground(User... users) {
-        Token token = new Token("", "");
+    private SSLContext CertIt() throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = new BufferedInputStream(new FileInputStream("/storage/emulated/0/rev-o-dates-social-chain.crt"));
+        Certificate ca;
         try {
-            // Делаем REST-запрос
-            RestApi service = Retrorest.getRetrofitInstance().create(RestApi.class);
-            Call<Token> call = service.Login(users[0]);
-
-            try {
-                Response<Token> response = call.execute();
-                if (response.isSuccessful()){
-                    token = response.body();
-                }
-            }catch (Exception e) {
-                ;
-            }
-        } catch (Exception e) {
-            this.exception = e;
-
+            ca = cf.generateCertificate(caInput);
+            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+        } finally {
+            caInput.close();
         }
+
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, tmf.getTrustManagers(), null);
+
+// Tell the URLConnection to use a SocketFactory from our SSLContext
+        return context;
+    }
+
+    protected Token doInBackground(LOgin... users) {
+        Token token = new Token("", "");
+
+        try {
+            OkHttpClient client = new OkHttpClient.Builder().
+                    sslSocketFactory(CertIt().getSocketFactory()).build();
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("name", users[0].getName())
+                    .addFormDataPart("password", users[0].getPassword())
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(BASE_URL + "/api/auth/login/")
+                    .post(requestBody)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                if(response.isSuccessful()){
+                    String jsonData = response.body().string();
+                    JSONObject Jobject = new JSONObject(jsonData);
+
+                    token.setAccess(Jobject.getString("access"));
+                    token.setRefresh(Jobject.getString("refresh"));
+                }
+                // Do something with the response.
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
         // Если произошла ошибка, вернется пустой токен, иначе нормальный
         return token;
     }
